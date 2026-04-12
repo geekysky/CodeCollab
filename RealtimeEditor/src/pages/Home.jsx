423b581fe372c5214b29fa5cfbdb6ed53b6f7689
geekysky <debu69980@gmail.com>
2026-04-12 18:21:28 +0530

    feat: initialize project structure and implement home page UI with room creation logic

--- RealtimeEditor/src/pages/Home.jsx
+++ RealtimeEditor/src/pages/Home.jsx
@@ -1,22 +1,39 @@
 import React from 'react'
+import { v4 as uuidv4 } from 'uuid';
+import { useState } from 'react';
 
 function Home() {
+
+  const [roomId, setRoomId] = useState('');
+  const [username, setUsername] = useState('');
+
+  const createnewRoom = async (e) => {
+    e.preventDefault();
+    const id = uuidv4();
+
+    setRoomId(id);
+    console.log(id);
+  }
+
   return (
     <div className='homePageWrapper'>
       <div className='formWrapper'>
-        <img src='/code-sync.png' alt='code-sync-logo'></img>
+        <img className='homePageLogo' src='/code-sync.png' alt='code-sync-logo'></img>
         <h4 className='mainLabel'>Paste Invitation Room ID</h4>
         <div className='inputGroup'>
-          <input type="text" className='inputBox' placeholder='Room ID' />
-          <input type="text" className='inputBox' placeholder='Username' />
+          <input type="text" className='inputBox' placeholder='Room ID' value={roomId} onChange={(e) => setRoomId(e.target.value)} />
+          <input type="text" className='inputBox' placeholder='Username' value={username} onChange={(e) => setUsername(e.target.value)} />
           <button className='btn joinBtn'>Join</button>
 
           <span className='createInfo'>
             If you don't have an invite then create &nbsp;
-            <a href="" className='createNewBtn'>New Room</a>
+            <a onClick={createnewRoom} href="" className='createNewBtn'>New Room</a>
           </span>
         </div>
       </div>
+      <footer>
+        <h4>Built with ❤️&nbsp; by <a href="https://github.com/geekysky">Debjyoti</a></h4>
+      </footer>
     </div>
   )
 }
